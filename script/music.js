ed5302ec23c936900c3ce82b5966d425381240ed
aiprogrammerdeveloper <aiprogrammerdeveloper@gmail.com>
2024-12-21 17:34:09 +0800

    Update music.js

--- script/music.js
+++ script/music.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports.config = {
 	name: "music",
 	version: "1.0.0",
-	credits: "cliff",
+	credits: "Armenion",
 	role: 0,
 	aliases: ['play'],
 	cooldown: 0,
